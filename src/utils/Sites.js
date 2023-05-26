e98003cf6b00167b5ef8a72188b2885d08ddd97d
smulch <mulcahy.samuel@gmail.com>
2023-05-25 20:03:47 -0400

    photos to cards and adds historwe

--- src/utils/Sites.js
+++ src/utils/Sites.js
@@ -1,10 +1,19 @@
 // this file is to provide an array of objexts containing information about the sites that I'm highlighting with the portfolio
 
+import gameboxd from "../assets/images/gameboxd.png";
+import historwe from "../assets/images/historwe.png";
+
 const sites = [
     {
         title: "Gameboxd",
         text: "A team project made during coding bootcamp. A social media and review website for video games.",
-        image: "https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png",
+        image: gameboxd,
+        link: "https://gameboxd.herokuapp.com/"
+    },
+    {
+        title: "Historwe",
+        text: "A team project made during coding bootcamp. A simple social media focused on historical events.",
+        image: historwe,
         link: "https://gameboxd.herokuapp.com/"
     }
 ];
