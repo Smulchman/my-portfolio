26a50374c4ac1608076eb75e7e52271734237e5b
smulch <mulcahy.samuel@gmail.com>
2023-06-13 16:26:01 -0400

    first run at pdf implementation

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,28 +1,29 @@
 // This page is meant to simply display a pdf of my resume
 // React
-import React from 'react';
+import React from "react";
 // Core viewer
-import { Viewer } from '@react-pdf-viewer/core';
+import { Viewer } from "@react-pdf-viewer/core";
 
 // Web worker
-import { Worker } from '@react-pdf-viewer/core';
+import { Worker } from "@react-pdf-viewer/core";
 
 // Plugins
 // import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 
 // Import styles
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
 // Create new plugin instance
-const PluginInstance = defaultLayoutPlugin();
+// const PluginInstance = defaultLayoutPlugin();
 
 function Hello() {
-  
   return (
-    <div className="pdf">
-      <h2>placeholder</h2>
-    </div>
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+      <div className="pdf">
+        <Viewer fileUrl="../assets/mulcahy_resume.pdf" />
+      </div>
+    </Worker>
   );
 }
 
