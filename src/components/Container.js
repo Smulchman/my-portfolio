f1832b5cdd61098e428440b914f256a468baa874
smulch <mulcahy.samuel@gmail.com>
2023-06-22 14:49:56 -0400

    first attempt at adding transitions between pages

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,37 +1,93 @@
-import React, { useState } from 'react';
-import NavTabs from './NavTabs';
-import Home from '../pages/Home';
-import Projects from '../pages/Projects';
+import React, { useState } from "react";
+import NavTabs from "./NavTabs";
+import Home from "../pages/Home";
+import Projects from "../pages/Projects";
 // import Photography from '../pages/Photography';
-import Resume from '../pages/Resume';
-import Contact from '../pages/Contact';
+import Resume from "../pages/Resume";
+import Contact from "../pages/Contact";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function PortfolioContainer() {
-    const [currentPage, setCurrentPage] = useState('Home');
-  
-    // Render a page based on the state value of `currentPage`
-    const renderPage = () => {
-      if (currentPage === 'Contact') {
-        return <Contact />;
-      }
-      if (currentPage === 'Projects') {
-        return <Projects />;
-      }
-      if (currentPage === 'Photography') {
-        return <Photography />;
-      }
-      if (currentPage === 'Resume') {
-        return <Resume />;
-      }
-      return <Home />;
-    };
-  
-    const handlePageChange = (page) => setCurrentPage(page);
-  
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  // Render a page based on the state value of `currentPage`
+  const renderPage = () => {
+    if (currentPage === "Contact") {
+      return (
+        <AnimatePresence>
+          <motion.div
+            key="contact"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <Contact />;
+          </motion.div>
+        </AnimatePresence>
+      );
+    }
+    if (currentPage === "Projects") {
+      return (
+        <AnimatePresence>
+          <motion.div
+            key="projects"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <Projects />
+          </motion.div>
+        </AnimatePresence>
+      );
+    }
+    if (currentPage === "Photography") {
+      return (
+        <AnimatePresence>
+          <motion.div
+            key="photography"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <Photography />
+          </motion.div>
+        </AnimatePresence>
+      );
+    }
+    if (currentPage === "Resume") {
+      return (
+        <AnimatePresence>
+          <motion.div
+            key="resume"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <Resume />
+          </motion.div>
+        </AnimatePresence>
+      );
+    }
     return (
-      <div>
-        <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-        {renderPage()}
-      </div>
+      <AnimatePresence>
+        <motion.div
+          key="home"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <Home />
+        </motion.div>
+      </AnimatePresence>
     );
-  }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
